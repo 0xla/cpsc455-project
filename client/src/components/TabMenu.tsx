a7c5c5369a7ef41765f2b42f86e57a3df2f3e9a6
steven wong <wong.steven604@gmail.com>
2022-06-14 20:21:11 -0700

    tab toggle component

---
+++ client/src/components/TabMenu.tsx
@@ -0,0 +1,15 @@
+import { Paper, Tab, Tabs } from "@mui/material";
+import "../App/App.css"
+
+export default function TabMenu({ option, optionChange }: { option: number, optionChange: any }) {
+    return (
+        <Paper square className="Tabs__main">
+            <Tabs value={option} onChange={optionChange} centered>
+                <Tab label="Photos" />
+                <Tab label="Analysis" />
+            </Tabs>
+        </Paper>
+
+    );
+
+}
--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -3,17 +3,27 @@ import Navigation from "../components/Navigation";
 import Box from "@mui/material/Box";
 import ImageCard from "../components/ImageCard";
 import { images } from "../dummydata/imagesPlaceholder";
+import { useState } from "react";
+import TabMenu from "../components/TabMenu";
 
 
 const Homepage = () => {
+    const [option, setOption] = useState(0);
+
+    const optionChange = (_event: any, selected: number) => {
+        setOption(selected);
+    };
     return (
         <Box>
             <Box mt={5}>
                 <Navigation />
                 <ImageUpload />
+                <Box mt={2}>
+                    <TabMenu option={option} optionChange={optionChange} />
+                </Box>
             </Box>
             <Box mt={5}>
-                {images.map((image: any, key) => (
+                {option === 0 && images.map((image: any, key) => (
                     <Box mt={2}>
                         <ImageCard imageData={image} />
                     </Box>
