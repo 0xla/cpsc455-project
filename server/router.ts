a271cc5c7b7332cfea0145632dce5cc90bda5c4e
steven wong <wong.steven604@gmail.com>
2022-06-27 16:46:45 -0700

    upload image to google cloud bucket

--- server/router.ts
+++ server/router.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { loginUser, registerUser } from "./controllers/authControllers";
-import { addImage, deleteImage, getImages, editImage } from "./controllers/imageController";
+import { uploadImage, deleteImage, getImages, editImage } from "./controllers/imageController";
 import { protect } from "./middleware/auth";
 
 const router = express.Router();
@@ -16,9 +16,9 @@ router.post("/api/users/login", loginUser);
  * IMAGE ENDPOINTS
  */
 
-router.post("api/images", addImage);
-router.get("api/images", getImages);
-router.put("api/images/:imageId", editImage);
-router.delete("api/images/:imageId", deleteImage);
+router.post("/api/images", uploadImage);
+router.get("/api/images", getImages);
+router.put("/api/images/:imageId", editImage);
+router.delete("/api/images/:imageId", deleteImage);
 
 export default router;
--- server/server.ts
+++ server/server.ts
@@ -1,20 +1,20 @@
 import router from "./router";
-import express from "express"; // Express web server framework
-import cors from "cors"; // Allows for Cross Origin Resource Sharing
-import mongoose from "mongoose"; //Mongoose is a MongoDB library
-import dotenv from "dotenv"; // .env for environment variables
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bodyParser from 'body-parser';
+import multer from 'multer';
 
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-app.use("/", router)
+app.use("/", router);
 
-//Get uri from environment variables
 const uri = process.env.ATLAS_URI;
 
-//Start MongoDB connection
 // @ts-ignore
 mongoose.connect(uri, {
   useNewUrlParser: true,
@@ -25,9 +25,6 @@ mongoose.connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
- 
 app.listen(port, () => {
-  // perform a database connection when server starts
-
   console.log(`Server is running on port: ${port}`);
 });
