98925960a20602965e359d2d22bddea530a57cda
UBC Student <lakshy01@students.cs.ubc.ca>
2022-06-28 11:37:07 -0700

    added the following followers UI components, not functional as of now when clicked on at the moment

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,7 +1,5 @@
 import ImageUpload from "../components/ImageUpload";
-import Navigation from "../components/Navigation";
 import TopNavigation from "../components/TopNavigation";
-import Box from "@mui/material/Box";
 import ImageCard from "../components/ImageCard";
 import { useState } from "react";
 import TabMenu from "../components/TabMenu";
@@ -20,22 +18,46 @@ const Homepage = () => {
 
     const [option, setOption] = useState(0);
 
+    const name : string = "user name";
+
     const optionChange = (_event: any, selected: number) => {
         setOption(selected);
     };
-    // sx={{width: 50, height: 50}}
     return (
         <div className="bg-[#FAFAFA] ">
             <div>
                 <TopNavigation />
                 <div className="flex flex-row items-center mx-[10vw]">
-                    <div className="flex flex-col mr-[100px]">
+                    <div className="flex flex-col mr-[100px] p-2">
                         <img className="flex-none md:w-[200px] md:h-[200px] w-[100px] h-[100px] rounded-full p-2" alt="Remy Sharp" src={profileImageUrl} />
                     </div>
-                    <div>
-                        {/* put followers following */}
+                    <div className="flex flex-col items-start gap-[15px] mr-[100px]">
+                        <div className="flex flex-row gap-[30px]">
+                            <div className="text-xl">
+                                {name}
+                            </div>
+                            <div className="border-[2px] py-[0.5px] px-[5px] border-gray-400 rounded hover:cursor-pointer text-base font-medium">
+                                Follow
+                            </div>
+                        </div>
+                        <div className="flex flex-row gap-[50px]">
+                            <div className="">
+                                <span className="font-bold">50</span> posts
+                            </div>
+                            <div className="">
+                                <span className="font-bold">1.2M</span> followers
+                            </div>
+                            <div className="">
+                                <span className="font-bold">5K</span> following 
+                            </div>
+                        </div>
+                        <div className="flex flex-col items-start">
+                            <span className="font-bold">Instagram's {name}</span>
+                            <div>{userBio}</div>
+                        </div>
                     </div>
                     <ImageUpload />
+
                 </div>
                 <div className="mt-2">
                     <TabMenu option={option} optionChange={optionChange} />
