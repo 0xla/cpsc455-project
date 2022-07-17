411bc34cd5611331ac97bf3e086dc70f57dd5f71
Daniel Crookall <dancrookall@gmail.com>
2022-07-17 00:31:11 -0700

    Finish like/unlike

--- client/src/components/ImageCard.tsx
+++ client/src/components/ImageCard.tsx
@@ -1,60 +1,17 @@
 import { ImageData } from "../types";
-
 import Box from "@mui/material/Box";
-
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import ThumbUpOffAltOutlinedIcon from '@mui/icons-material/ThumbUpOffAltOutlined';
-
-import ThumbDownIcon from '@mui/icons-material/ThumbDown';
-import ThumbDownOffAltOutlinedIcon from '@mui/icons-material/ThumbDownOffAltOutlined';
-
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-
-
-import {useEffect, useState} from "react";
 import Typography from "@mui/material/Typography";
 import axios from "axios";
 import {selectUserData, setImages} from "../slices/userSlice";
 import {useDispatch, useSelector} from "react-redux";
 
 export default function ImageCard({ imageData }: { imageData: ImageData }) {
-    const [isLiked, setIsLiked] = useState(false)
     const dispatch = useDispatch();
     const {userId} = useSelector(selectUserData);
 
-
-    const likePost = async (postId: number, userId: string) => {
-
-        try {
-            const res = await axios.put(
-                `http://localhost:5000/api/posts/${postId}/likes/${userId}`,
-            );
-            dispatch(setImages(res.data.data));
-            setIsLiked(true)
-            console.log(res.data.data)
-        } catch (err: any) {
-
-        }
-    }
-
-    const unlikePost = async (postId: number, userId: string) => {
-
-        try {
-            const res = await axios.delete(
-                `http://localhost:5000/api/posts/${postId}/likes/${userId}`,
-            );
-            dispatch(setImages(res.data.data));
-            setIsLiked(false)
-            console.log(res)
-        } catch (err: any) {
-
-        }
-    }
-
     const handleLike = async (postId: string, userId: string) => {
-        console.log(imageData)
-        console.log("postid " + postId)
         if (imageData.likes.includes(userId)) {
             try {
                 const res = await axios.delete(
@@ -62,42 +19,32 @@ export default function ImageCard({ imageData }: { imageData: ImageData }) {
                 );
                 dispatch(setImages(res.data.data));
             } catch (err: any) {
-
+                console.log("Error unliking post.")
             }
         } else {
             try {
                 const res = await axios.put(
                     `http://localhost:5000/api/posts/${postId}/likes/${userId}`,
                 );
-                console.log(res.data.data)
                 dispatch(setImages(res.data.data));
 
             } catch (err: any) {
-                console.log("error liking")
-
+                console.log("Error liking post.")
             }
         }
     }
-
-
-
 
     return (
         <div className="card w-auto bg-base-100 shadow-xl">
             <figure><img src={imageData.url}  alt={imageData.id}/></figure>
             <Box sx={{paddingTop: 5, paddingLeft: 5, display: 'flex',}}>
 
-                {/*<FavoriteBorderIcon style={{ color: 'rgb(255,0,0)' }} onClick={() => likePost(1,2)}/>*/}
-
-                {imageData.likes.includes(userId) ? <FavoriteIcon style={{ color: 'rgb(255,0,0)' }} onClick={() =>handleLike(imageData.id,userId)}/> : <FavoriteBorderIcon onClick={() => handleLike(imageData.id,userId)}/>}
-
-
-                {/*<ThumbUpIcon onClick={() => likePost(1,"62a54042cf860174904de5de")}/>*/}
-                {/*<ThumbDownIcon onClick={() => unlikePost(1,"62a54042cf860174904de5de")}/>*/}
-                {/*{upvote ? <ThumbUpIcon onClick={likePost(1,2)}/> : <ThumbUpOffAltOutlinedIcon onClick={handleUpvote} />}*/}
-                {/*{downvote ? <ThumbDownIcon onClick={handleDownvote}/> : <ThumbDownOffAltOutlinedIcon onClick={handleDownvote} />}*/}
+                {imageData.likes.includes(userId) ? <FavoriteIcon  style={{ color: 'rgb(255,0,0)' }}
+                                                                   onClick={() =>handleLike(imageData.id,userId)}/>
+                    : <FavoriteBorderIcon onClick={() => handleLike(imageData.id,userId)}/>}
             </Box>
-            <Typography align="left" sx={{paddingTop: 1, paddingLeft: 6}}>{imageData.likes.length} {imageData.likes.length === 1 ? "like" : "likes"} </Typography>
+            <Typography align="left" sx={{paddingTop: 1, paddingLeft: 6}}>{imageData.likes.length}
+                {imageData.likes.length === 1 ? " like" : " likes"} </Typography>
             <div className="card-body">
                 <p>{imageData.description}</p>
             </div>
--- server/controllers/imageController.ts
+++ server/controllers/imageController.ts
@@ -5,7 +5,6 @@ import util from "util";
 import { Storage } from "@google-cloud/storage";
 import { ObjectId } from "mongodb";
 import { v4 as uuidv4 } from "uuid";
-import USER_ERR from "../errors/userErrors";
 import dotenv from "dotenv";
 dotenv.config()
 const storage = new Storage({ keyFilename: "google-cloud-key.json" });
@@ -83,43 +82,30 @@ export const uploadImage = async (req: Request, res: Response) => {
 
 export const likePost = async (req: Request, res: Response) => {
   const {postid, userid} = req.params;
-  console.log("user id" + userid)
-  console.log("postid" + postid)
   let result;
 
-  const voteRecord = await User.find(
-      {
-        $and: [{ 'images.likes': userid }, { 'images.id': postid}],
-        // 'images.likes' : userid
-      })
-
-  console.log("vote record is" + voteRecord)
-
-  if (voteRecord.length !== 0){
-    return res.status(400).json({
-      message: "User has already voted on this post.",
-    });
-  }
-
   try {
-    result = await User.findOneAndUpdate(
-        {'images.id': postid},
+    result = await User.findOneAndUpdate({
+          "images.id": postid
+        },
         {
-          $push: {
-            'images.$[].likes':
-            userid
+          $addToSet: {
+            "images.$[images].likes": userid
           }
-        },{ arrayFilters: [{ 'images.id': postid }], new: true}
-    )
+        },
+        {
+          arrayFilters: [
+            {
+              "images.id": postid
+            }
+          ], new: true
+        })
   } catch (err) {
     return res.status(400).json({
       message: "Error liking post",
       error: err,
     });
   }
-
-
-
 
   return res.status(200).json({
     message: "Successfully liked post",
@@ -139,17 +125,21 @@ export const unlikePost = async (req: Request, res: Response) => {
   let result;
 
   try {
-    result = await User.findOneAndUpdate(
-        {'images.id': postid},
+    result = await User.findOneAndUpdate({
+          "images.id": postid
+        },
         {
           $pull: {
-            'images.$[].likes':
-            userid
+            "images.$[images].likes": userid
           }
-        }, {
-          new: true
-        }
-    )
+        },
+        {
+          arrayFilters: [
+            {
+              "images.id": postid
+            }
+          ], new: true
+        })
   } catch (err) {
     res.status(400).json({
       message: "Error unliking post",
@@ -157,8 +147,6 @@ export const unlikePost = async (req: Request, res: Response) => {
     });
   }
 
-  console.log(result)
-  console.log("delete result is " + result.images)
   res.status(200).json({
     message: "Successfully disliked post",
     data: result.images,
