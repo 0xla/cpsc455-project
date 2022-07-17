44bac9d2bedca98868ee2477e4cea005f3044893
Daniel Crookall <dancrookall@gmail.com>
2022-07-17 01:01:11 -0700

    Edit like heart size

--- client/src/components/ImageCard.tsx
+++ client/src/components/ImageCard.tsx
@@ -7,6 +7,7 @@ import axios from "axios";
 import {selectUserData, setImages} from "../slices/userSlice";
 import {useDispatch, useSelector} from "react-redux";
 
+
 export default function ImageCard({ imageData }: { imageData: ImageData }) {
     const dispatch = useDispatch();
     const {userId} = useSelector(selectUserData);
@@ -39,9 +40,9 @@ export default function ImageCard({ imageData }: { imageData: ImageData }) {
             <figure><img src={imageData.url}  alt={imageData.id}/></figure>
             <Box sx={{paddingTop: 5, paddingLeft: 5, display: 'flex',}}>
 
-                {imageData.likes.includes(userId) ? <FavoriteIcon  style={{ color: 'rgb(255,0,0)' }}
+                {imageData.likes.includes(userId) ? <FavoriteIcon fontSize="large" style={{ color: 'rgb(255,0,0)' }}
                                                                    onClick={() =>handleLike(imageData.id,userId)}/>
-                    : <FavoriteBorderIcon onClick={() => handleLike(imageData.id,userId)}/>}
+                    : <FavoriteBorderIcon fontSize="large" sx={{ "&:hover": { color: "gray"} }} onClick={() => handleLike(imageData.id,userId)}/>}
             </Box>
             <Typography align="left" sx={{paddingTop: 1, paddingLeft: 6}}>{imageData.likes.length}
                 {imageData.likes.length === 1 ? " like" : " likes"} </Typography>
