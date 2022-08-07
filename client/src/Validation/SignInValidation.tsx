2b1b481aa62d30b296c156e9bed565d95ccdab16
Daniel Crookall <dancrookall@gmail.com>
2022-08-07 07:20:09 -0700

    Add spinner to signup page, update password validation text

--- client/src/Validation/SignInValidation.tsx
+++ client/src/Validation/SignInValidation.tsx
@@ -8,6 +8,6 @@ export const validationSchema = yup.object({
         .required('Please enter your password')
         .matches(
             /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-            "Must contain eight characters, one uppercase, one lowercase, one number and one special character"
+            "Must contain at least eight characters, one uppercase, one lowercase, one number and one special character"
         )
 })
